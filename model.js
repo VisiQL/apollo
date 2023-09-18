ef0e27777aa464d0b746220991538185da60db6c
kcuevas <cuev73@live.com>
2023-09-18 13:31:01 -0400

    feature branch esmodule converts all modules to esmodule with import/export statements

--- model.js
+++ model.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+// require('dotenv').config();
+import { Pool } from 'pg';
 
 let userPool = new Pool({
   connectionString:  'postgres://xstskucd:HUMKg1LzALryqlQM26N5uKLWF5ol1fbT@peanut.db.elephantsql.com/xstskucd',
 });
 
-module.exports = {
+export default {
   query: (text, params, callback) => {
     console.log('executed query: ', text);
     return userPool.query(text, params, callback);
